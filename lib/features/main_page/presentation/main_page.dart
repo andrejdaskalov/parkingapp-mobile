8f7271369a5850cc9cf24592c5f365e685404cb9
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2023-12-02 18:47:56 +0100

    added get_it and bloc dependencies; made basic folder structure and extracted scaffold to another file;

---
+++ lib/features/main_page/presentation/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+      appBar: AppBar(
+        title: const Text("ParkMe"),
+        backgroundColor: Colors.transparent,
+      ),
+      // body: ,
+    );
+  }
+}
