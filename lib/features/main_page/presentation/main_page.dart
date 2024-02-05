1eb81b38f3928ceda9dcc28d3b497d41218f2da8
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2024-02-05 18:59:10 +0100

    merged

--- lib/features/main_page/presentation/main_page.dart
+++ lib/features/main_page/presentation/main_page.dart
@@ -8,9 +8,22 @@ import 'package:parkingapp/core/repository/parking_repository.dart';
 import '../../../core/dependency_injection/injectable_config.dart';
 import 'bloc/main_page_bloc.dart';
 
-
 class MainPage extends StatelessWidget {
   MainPage({super.key});
+
+  var markers = <Marker>[
+    Marker(
+      point: LatLng(42.0041,21.4095),
+      width: 80,
+      height: 80,
+      child: Icon(Icons.location_on, color: Colors.red),
+    ),Marker(
+      point: LatLng(42.0035,21.4168),
+      width: 80,
+      height: 80,
+      child: Icon(Icons.location_on, color: Colors.green),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,35 +40,62 @@ class MainPage extends StatelessWidget {
           backgroundColor: Colors.transparent,
         ),
         body: Stack(
-          child: BlocProvider(
-            create: (context)  {
-              var instance = getIt.get<MainPageBloc>();
-              instance.add(GetPlaces());
-              return instance;
-            },
-            child: BlocBuilder<MainPageBloc, MainPageState>(
-              builder: (context, state) {
-                if (state.status == Status.loading) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state.status == Status.error) {
-                  return Center(child: Text(state.error));
-                } else {
-                  return ListView.builder(
-                    itemCount: state.places.length,
-                    itemBuilder: (context, index) {
-                      final place = state.places[index];
-                      return ListTile(
-                        title: Text(place.name),
-                        subtitle: Text(place.address),
-                      );
-                    },
-                  );
-                }
-              },
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Flexible(
+                    child: FlutterMap(
+                      options: MapOptions(
+                          initialCenter: LatLng(41.99646,21.43141),
+                          initialZoom: 13,
+                          interactionOptions: InteractionOptions(
+                            enableMultiFingerGestureRace: true,
+                            rotationThreshold: 20.0,
+                          )
+                      ),
+                      children: [
+                        TileLayer(
+                          urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: ['a','b','c'],
+                        ),
+                        MarkerLayer(
+                          markers: markers,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        )
-
-    );
+            BlocProvider(
+              create: (context) {
+                var instance = getIt.get<MainPageBloc>();
+                instance.add(GetPlaces());
+                return instance;
+              },
+              child: BlocBuilder<MainPageBloc, MainPageState>(
+                builder: (context, state) {
+                  if (state.status == Status.loading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state.status == Status.error) {
+                    return Center(child: Text(state.error));
+                  } else {
+                    return ListView.builder(
+                      itemCount: state.places.length,
+                      itemBuilder: (context, index) {
+                        final place = state.places[index];
+                        return ListTile(
+                          title: Text(place.name),
+                          subtitle: Text(place.address),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ));
   }
 }
