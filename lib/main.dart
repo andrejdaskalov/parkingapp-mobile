93f685d8dc9b3a4e6ac6f40c10de60fc6088ca51
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2024-03-23 23:25:38 +0100

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parkingapp/features/main_page/presentation/main_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-
 import 'core/dependency_injection/injectable_config.dart';
-import 'package:parkingapp/core/dependency_injection/injectable_config.dart';
 import 'package:flutter/services.dart';
 
 void main() {
