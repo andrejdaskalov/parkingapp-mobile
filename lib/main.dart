638e9d79d092dd542605d84a4566619472923033
Глигоров Глигорчо <gligorcho.gligorov@students.finki.ukim.mk>
2024-04-10 16:47:52 +0200

    aaaa

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:parkingapp/features/navbar/navbar.dart';
 import 'package:parkingapp/features/main_page/presentation/main_page.dart';
+import 'package:parkingapp/features/stop_button/stop_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'core/dependency_injection/injectable_config.dart';
 import 'package:flutter/services.dart';
 
 import 'features/theme/themes.dart';
+//
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter's binding is initialized
 
-void main() {
+  // Obtain shared preferences.
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('currentlyPayingParking', 'AB1234CD');
+  // Check if there is a value stored in 'currentlyPayingParking'.
+  final String? currentlyPayingParking = prefs.getString('currentlyPayingParking');
+
   String environment = appFlavor.toString();
   configureDependencies(environment);
-  runApp(const MyApp());
+  runApp(MyApp(currentlyPayingParking: currentlyPayingParking, prefs: prefs));
+
+
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String? currentlyPayingParking;
+  final SharedPreferences prefs;
+
+
+  const MyApp({Key? key, this.currentlyPayingParking, required this.prefs}) : super(key: key);
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-            routerConfig: GoRouter(routes: [
-          ShellRoute(
-              builder: (context, state, child) {
-                return Scaffold(
-                  body: child,
-                  bottomNavigationBar: NavBar(
-                    onRouteChanged: (String route) {
-                      // context.goNamed(route); TODO: uncomment after giving valid routes
-                    },
-                  ),
-                  extendBody: true,
-                );
-              },
-              routes: [
-                GoRoute(path: "/", builder: (context, state) => MainPage())
-              ]),
+      routerConfig: GoRouter(routes: [
+        ShellRoute(
+            builder: (context, state, child) {
+              return Scaffold(
+                body: child,
+                bottomNavigationBar: NavBar(
+                  onRouteChanged: (String route) {
+                    // context.goNamed(route); TODO: uncomment after giving valid routes
+                  },
+                ),
+                extendBody: true,
+                floatingActionButton: currentlyPayingParking != null
+                    ? StopButton(
 
-        ]),
+                  onPressed: () async {
+                    // Delete the value from shared preferences.
+                    // await prefs.remove('currentlyPayingParking');
+                  },
+                )
+                    : null,
+                floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+              );
+            },
+            routes: [
+              GoRoute(path: "/", builder: (context, state) => MainPage())
+            ]),
+
+      ]),
       title: 'ParkWise',
       theme: ThemeData(
         colorScheme: lightTheme,
@@ -50,3 +78,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
