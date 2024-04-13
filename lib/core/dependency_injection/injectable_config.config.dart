4d8db64d064be9f715a24c23d960bef6492ae287
Marija Gjorgjieva <82838042+marijagjorgjieva@users.noreply.github.com>
2024-04-13 13:39:34 +0200

    Changes in injectable config
    -  added for testing purposes

--- lib/core/dependency_injection/injectable_config.config.dart
+++ lib/core/dependency_injection/injectable_config.config.dart
@@ -10,12 +10,13 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-
-import '../../features/main_page/presentation/bloc/main_page_bloc.dart' as _i7;
 import '../api/dev/parking_api_dev.dart' as _i4;
 import '../api/parking_api.dart' as _i3;
 import '../api/prod/parking_api_prod.dart' as _i5;
 import '../repository/impl/parking_repository.dart' as _i6;
+import '../repository/impl/user_input_repository.dart' as _i7;
+import '../../features/main_page/presentation/bloc/main_page_bloc.dart' as _i8;
+
 
 const String _dev = 'dev';
 const String _prod = 'prod';
@@ -41,8 +42,12 @@ extension GetItInjectableX on _i1.GetIt {
     );
     gh.factory<_i6.ParkingRepository>(
         () => _i6.ParkingRepository(gh<_i3.ParkingApi>()));
-    gh.factory<_i7.MainPageBloc>(
-        () => _i7.MainPageBloc(gh<_i6.ParkingRepository>()));
+
+    gh.factory<_i7.UserInputRepository>(
+            () => _i7.UserInputRepository(gh<_i3.ParkingApi>(),gh<_i6.ParkingRepository>()));
+
+    gh.factory<_i8.MainPageBloc>(
+        () => _i8.MainPageBloc(gh<_i6.ParkingRepository>(),gh<_i7.UserInputRepository>()));
     return this;
   }
 }
--- lib/features/main_page/presentation/bloc/main_page_bloc.dart
+++ lib/features/main_page/presentation/bloc/main_page_bloc.dart
@@ -5,6 +5,7 @@ import 'package:equatable/equatable.dart';
 import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
 import 'package:parkingapp/core/repository/impl/parking_repository.dart';
+import 'package:parkingapp/core/repository/impl/user_input_repository.dart';
 
 import '../../../../core/domain/model/parking.dart';
 
@@ -14,15 +15,20 @@ part 'main_page_state.dart';
 @injectable
 class MainPageBloc extends Bloc<MainPageEvent, MainPageState> {
   final ParkingRepository _parkingRepository;
+  final UserInputRepository _userInputRepository;
 
-  MainPageBloc(this._parkingRepository) : super( MainPageState(status: Status.loading)){
+  MainPageBloc(this._parkingRepository, this._userInputRepository) : super( MainPageState(status: Status.loading)){
     on<GetPlaces>(_getPlaces);
   }
 
 
     Future<void> _getPlaces(GetPlaces event, Emitter<MainPageState> emit) async {
+    print('Fetching places...');
     try {
       final places = await _parkingRepository.listParkings();
+      print('Places fetched: $places');
+      final userInputs = await _userInputRepository.listUserInputs();
+      print('User inputs fetched: $userInputs');
       emit(state.copyWith(status: Status.loaded, places: places));
     } catch (e) {
       emit(state.copyWith(status: Status.error, error: e.toString()));
