774ceda71e5398e007ee31f6bf14a3fd69381e10
andrejdaskalov <41647331+andrejdaskalov@users.noreply.github.com>
2024-05-19 22:45:48 +0200

    added availability_slider.dart; changed details_card.dart with dynamic height based on children; added price field to details_card.dart;

--- lib/features/details/presentation/details_card.dart
+++ lib/features/details/presentation/details_card.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:parkingapp/core/domain/model/availability.dart';
 import 'package:parkingapp/features/common_ui/buttons.dart';
 import 'package:parkingapp/features/common_ui/headings.dart';
+import 'package:parkingapp/features/details/presentation/availability_slider.dart';
 import 'package:parkingapp/features/main_page/presentation/bloc/main_page_bloc.dart';
 
 import '../../../core/domain/model/parking.dart';
@@ -45,85 +46,87 @@ class DetailsCard extends StatelessWidget {
               },
               child: Container(
                 margin: EdgeInsets.all(10),
-                height: 300,
-                child: Card(
-                    color: Theme.of(context).colorScheme.background,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Flexible(
-                                child: Container(
-                                  margin: EdgeInsets.only(right: 10),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      TitleHeading(text: place!.name),
-                                      BoldHeading(text: place!.address),
-                                      SubtitleHeading(text: place!.type.name),
-                                      Text(availability.hasEntriesToday
-                                          ? "Пријавено денеска"
-                                          : "Типично за ова место"),
-                                      Text(
-                                          "Зафатеност: ${availability.averageOccupancy.toStringAsFixed(2)}%"),
-                                    ],
+                child: SingleChildScrollView(
+                  child: Card(
+                      color: Theme.of(context).colorScheme.background,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  child: Container(
+                                    margin: EdgeInsets.only(right: 10),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        TitleHeading(text: place!.name),
+                                        BoldHeading(text: place!.address),
+                                        Container(height: 10,),
+                                        SubtitleHeading(text: place!.type == ParkingType.zoned ? "Зонско паркирање" : "Слободно паркирање"),
+                                        AvailabilitySlider(availability: availability ),
+                                        Container(height: 10,),
+                                        SubtitleHeading(text: "Цена: ${place!.pricePerHour} ден/час"),
+                                        Container(height: 25,),
+
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  ZoneLabel(text: "Зона"),
-                                  ZoneHeading(text: place!.zone.toString()),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  PrimaryButtonWithIcon(
-                                      icon: Icons.local_parking,
-                                      text: "Започни паркинг",
-                                      onPressed: onPay)
-                                ],
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    height: 5,
-                                    width: 100,
-                                    decoration: BoxDecoration(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onBackground
-                                          .withOpacity(0.5),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    )),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ZoneLabel(text: "Зона"),
+                                    ZoneHeading(text: place!.zone.toString()),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    PrimaryButtonWithIcon(
+                                        icon: Icons.local_parking,
+                                        text: "Започни паркинг",
+                                        onPressed: onPay)
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: 5,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onBackground
+                                            .withOpacity(0.5),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+                ),
               ),
             );
         },
